7afff8f5613a9fda7ca92b49e8ef24c11182a126
alex <tangyongjin@gmail.com>
2020-12-05 18:42:46 +0800

    chage  api_config

---
+++ src/api/api_config_v2/base_config.js
@@ -0,0 +1,36 @@
+let root_url = 'http://127.0.0.1';
+let port = '8502';
+const version_2 = 'v2';
+const controller = {
+    sales_api: 'sales_api',
+    contract_api: 'contract_api',
+    cabinet_api: 'cabinet_api',
+    menu_api: 'menu_api',
+    bpm: 'bpm',
+    process: 'process',
+    startTask: "start_work",
+    cabinet: "sinnet_tree_plugin",
+    resitemui: "resitemui",
+    permission: 'permission',
+    equipmentMaterial: 'equipmentMaterial',
+    completion: 'completion',
+    billing_api: 'billing_api',
+    activity: 'Activity',
+    curd: 'curd',
+    auth: 'Auth',
+    file: 'File',
+    button: 'Button',
+    organization: 'organization',
+    user: 'user',
+    address: 'address',
+    resourcesOrder: 'ResourcesOrder',
+    notify: 'notify'
+}
+
+export {
+    version_2,
+    root_url,
+    port,
+    controller
+}
+
--- src/api/api_customer.js
+++ src/api/api_customer.js
@@ -7,9 +7,7 @@ const api_root = `${ root_url }:${ port }/${ version_2 }`
 export default class customer {
     static apis = {
           inquiryCompanyMsg: (params) => http(params, `${ api_root }/Sales_api/inquiryCompanyMsg`),
-        // 根据客户id获取客户地址
-         // 提交地址，待审批
-         inquiryCompanyList: (params) => http(params, `${ api_root }/Sales_api/inquiryCompanyList`),
+          inquiryCompanyList: (params) => http(params, `${ api_root }/Sales_api/inquiryCompanyList`),
       }
 }
 
