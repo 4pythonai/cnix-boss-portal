b55b503dceb06824e76e4fcd99b6dda5df69bc3c
alex <tangyongjin@gmail.com>
2021-02-15 11:32:35 +0800

    上传excel add loading effect

--- src/routes/commonAntTable/components/table_plugins/bill/UploadBankExcel.js
+++ src/routes/commonAntTable/components/table_plugins/bill/UploadBankExcel.js
@@ -1,4 +1,4 @@
-import { Modal, message, Upload, Icon, Button } from 'antd';
+import { Modal, message, Upload, Spin, Icon, Button } from 'antd';
 import { observer } from 'mobx-react';
 import api from '@/api/api';
 import React from 'react';
@@ -8,19 +8,33 @@ export default class UploadBankExcel extends React.Component {
     constructor(props) {
         super(props);
         this.init = this.init.bind(this);
+        this.getExcelUrl = this.getExcelUrl.bind(this);
+        this.getModalProps = this.getModalProps.bind(this);
+        this.showLoading = this.showLoading.bind(this);
+        this.hideLoading = this.hideLoading.bind(this);
     }
 
     state = {
-        visible: false
+        visible: false,
+        working: false
     };
 
     init() {
-        this.setState({ visible: true });
+        this.setState({ visible: true, working: false });
     }
+
+    showLoading = () => {
+        this.setState({ working: true });
+    };
+
+    hideLoading = () => {
+        this.setState({ working: false });
+    };
 
     onCancel = () => {
         this.setState({
-            visible: false
+            visible: false,
+            working: false
         });
     };
 
@@ -29,19 +43,24 @@ export default class UploadBankExcel extends React.Component {
         const props = {
             name: 'file',
             action: api.filehandler.uploadBankExcel,
+            loading: true,
             accept: 'application/vnd.ms-excel',
             headers: {
                 Authorization: localStorage.getItem('token')
             },
             onChange(info) {
+                that.showLoading();
                 console.log('上传结果');
                 console.log(info);
 
                 if (info.file.status === 'error') {
+                    that.hideLoading();
                     message.success(`${info.file.name} 上传失败！`);
                 }
 
                 if (info.file.status === 'done') {
+                    that.hideLoading();
+
                     if (info.file.response.code === 200) {
                         message.success(`${info.file.name} 上传成功！`);
                         that.props.refreshTable();
@@ -81,7 +100,10 @@ export default class UploadBankExcel extends React.Component {
                 <div>
                     <div>重复上传文件会导致数据库"数据重复错误"</div>
                     <div>"文件名"+"交易时间"必须唯一</div>
+                    <br />
 
+                    {this.state.working ? <Spin tip="上传中..."></Spin> : ''}
+                    <br />
                     <br />
 
                     <Upload {...this.getExcelUrl()} showUploadList={false}>
