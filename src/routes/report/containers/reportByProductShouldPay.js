290dc39e43f1685642a722f55f75d5cb72c08d77
alex <tangyongjin@gmail.com>
2021-09-07 00:24:56 +0800

    产品报告格式

--- src/routes/report/containers/reportByProductShouldPay.js
+++ src/routes/report/containers/reportByProductShouldPay.js
@@ -7,7 +7,22 @@ export default class reportByProductShouldPay extends React.Component {
     constructor(props) {
         super(props);
         this.reportrowsHander = this.reportrowsHander.bind(this);
+        this.renderMoneyAndProductNum = this.renderMoneyAndProductNum.bind(this);
     }
+
+    renderMoneyAndProductNum = (text, record) => {
+        if (text.includes('/')) {
+            const tmparr = text.split('/');
+            return (
+                <div>
+                    <div style={{ whiteSpace: 'nowrap' }}> 金额:{tmparr[0]}</div>
+                    <div style={{ borderTop: '0.5px solid black', whiteSpace: 'nowrap' }}> 数量:{tmparr[1]}</div>
+                </div>
+            );
+        } else {
+            return <div>{text}</div>;
+        }
+    };
 
     reportrowsHander(_rptrows) {
         this.setState({
@@ -44,13 +59,6 @@ export default class reportByProductShouldPay extends React.Component {
                 dataIndex: 'product_name',
                 key: 'product_name'
             },
-
-            {
-                title: '数量',
-                dataIndex: 'counter',
-                key: 'counter'
-            },
-
             {
                 title: '[1-12月合计]',
                 dataIndex: 'm_total',
@@ -60,62 +68,74 @@ export default class reportByProductShouldPay extends React.Component {
             {
                 title: '1月应收',
                 dataIndex: 'm1',
-                key: 'm1'
+                key: 'm1',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '2月应收',
                 dataIndex: 'm2',
-                key: 'm2'
+                key: 'm2',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '3月应收',
                 dataIndex: 'm3',
-                key: 'm3'
+                key: 'm3',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '4月应收',
                 dataIndex: 'm4',
-                key: 'm4'
+                key: 'm4',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '5月应收',
                 dataIndex: 'm5',
-                key: 'm5'
+                key: 'm5',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '6月应收',
                 dataIndex: 'm6',
-                key: 'm6'
+                key: 'm6',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '7月应收',
                 dataIndex: 'm7',
-                key: 'm7'
+                key: 'm7',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '8月应收',
                 dataIndex: 'm8',
-                key: 'm8'
+                key: 'm8',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '9月应收',
                 dataIndex: 'm9',
-                key: 'm9'
+                key: 'm9',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '10月应收',
                 dataIndex: 'm10',
-                key: 'm10'
+                key: 'm10',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '11月应收',
                 dataIndex: 'm11',
-                key: 'm11'
+                key: 'm11',
+                render: this.renderMoneyAndProductNum
             },
             {
                 title: '12月应收',
                 dataIndex: 'm12',
-                key: 'm12'
+                key: 'm12',
+                render: this.renderMoneyAndProductNum
             }
         ],
         reportrows: []
