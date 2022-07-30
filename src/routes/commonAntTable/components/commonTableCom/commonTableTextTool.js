5c25a9ce391ece592c65d1cb7632301e2b64cb7d
alex <tangyongjin@gmail.com>
2022-07-30 15:01:02 +0800

    列宽自动适应

---
+++ src/routes/commonAntTable/components/commonTableCom/commonTableTextTool.js
@@ -0,0 +1,19 @@
+export default function getTextWidth(text) {
+        const canvas = document.createElement('canvas');
+        let context = canvas.getContext('2d');
+        context.font = '14px Microsoft YaHei';
+        let textmetrics = context.measureText(text);
+        console.log(text);
+        console.log(textmetrics.width);
+        return textmetrics.width;
+}
+
+// export default function getMaxWidth(text) {
+//         const canvas = document.createElement('canvas');
+//         let context = canvas.getContext('2d');
+//         context.font = '14px Microsoft YaHei';
+//         let textmetrics = context.measureText(text);
+//         console.log(text);
+//         console.log(textmetrics.width);
+//         return textmetrics.width;
+// }
