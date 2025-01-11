8f4a1537c65d88a5becd17452639264dd929caf4
4python@ai <4python@ai>
2025-01-11 10:26:12 +0800

    clean code

--- DDOpenVue/components/common/cabinettree/index.js
+++
@@ -1,9 +0,0 @@
-import Cabinettree from './src/cabinettree.vue';
-
-/* istanbul ignore next */
-Cabinettree.install = function (Vue)
-{
-  Vue.component('cabinettree', Cabinettree);
-};
-
-export default Cabinettree;
--- DDOpenVue/components/common/cabinettree/indexAll.js
+++
@@ -1,8 +0,0 @@
-import CabinettreeAll from './src/cabinettreeall.vue';
-
-/* istanbul ignore next */
-CabinettreeAll.install = function(Vue) {
-    Vue.component('cabinettreeall', CabinettreeAll);
-};
-
-export default CabinettreeAll;
